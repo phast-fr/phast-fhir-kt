afd5a413870469943e2942dc2991aaabef01eb78
David Ouagne <david.ouagne@phast.fr>
2021-10-20 19:05:22 +0200

    chore(test): add test post search

---
+++ src/test/kotlin/org/hl7/fhir/r4/client/rest/RestClientTest.kt
@@ -0,0 +1,43 @@
+package org.hl7.fhir.r4.client.rest
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import org.hl7.fhir.r4.model.Coding
+import org.hl7.fhir.r4.model.UnsignedIntType
+import org.hl7.fhir.r4.model.ValueSetContains
+import org.junit.jupiter.api.Test
+
+
+class RestClientTest {
+
+    @Test
+    fun fhirSearchPost() {
+        val mapper = jacksonObjectMapper()
+        val content = RestClient::class.java.classLoader.getResource("valueset acure.json")
+        if (content != null) {
+            val codes = mapper.readValue<List<ValueSetContains>>(
+                content.readText()
+            )
+            val client = RestClient("https://api.logicahealth.org/Le23/open")
+            val bundle = client
+                .search()
+                .withResourceType("Condition")
+                .withSubject("phast-14602")
+                .withCodes(codes.map { valueSetContains ->
+                    Coding().also { coding ->
+                        coding.system = valueSetContains.system
+                        coding.display = valueSetContains.display
+                        coding.code = valueSetContains.code
+                    }
+                })
+                .execute()
+                .block()
+                ?.body
+            assert(bundle?.total == UnsignedIntType(0))
+        }
+        else {
+            assert(false)
+        }
+    }
+
+}
